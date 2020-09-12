a996674090b18c4be08ad4083a04a4cf6dddd58b
Shavkat Nasirov <aka.nosirov@gmail.com>
2020-09-12 21:00:41 +0900

    customized user serializer

--- rest_social_auth/serializers.py
+++ rest_social_auth/serializers.py
@@ -24,10 +24,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        exclude = (
-            'is_staff', 'is_active', 'date_joined', 'password', 'last_login', 'user_permissions',
-            'groups', 'is_superuser',
-        )
+        fields = ["id", "email", "username", "last_login"]
 
 
 class TokenSerializer(serializers.Serializer):
